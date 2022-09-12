369e3f2a9609cbb8ce06b3b0005c44096857d61d
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-12 20:11:16 +0600

    commit is done

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Set<City> cities=new TreeSet<>();
-
         City city1 =new City(6,"osh");
         City city2 =new City(7,"bishkek");
         City city3 =new City(5,"jalal_abat");
-
 
         if (city1.code%2!=0){
             cities.add(city1);
