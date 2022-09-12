92fe77196eb568977fa205185d769967703d1d6b
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-12 20:05:47 +0600

    a

--- src/City.java
+++ src/City.java
@@ -1,33 +1,20 @@
-public class City {
+public class City implements Comparable<City> {
     int code;
     String name;
 
     public City(int code, String name) {
-        this.code = code;
         this.name = name;
+        this.code=code;
     }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
-        return "City{" +
-                "code=" + code +
-                ", name='" + name + '\'' +
-                '}';
+        return  "  City->" +
+                " code-=-" + code +
+                " name-=-" + name+"\n"+
+              "--------------------------------------\n";
+    }
+    @Override
+    public int compareTo(City o) {
+        return o.code -  this.code ;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> cities=new TreeSet<>(Comparator.reverseOrder());
+        Set<City> cities=new TreeSet<>();
+
         City city1 =new City(6,"osh");
         City city2 =new City(7,"bishkek");
         City city3 =new City(5,"jalal_abat");
 
+
         if (city1.code%2!=0){
-            cities.add(city1.getCode());
+            cities.add(city1);
         }
         if (city2.code%2!=0){
-            cities.add(city2.getCode());
+            cities.add(city2);
         }
         if (city3.code%2!=0){
-            cities.add(city3.getCode());
+            cities.add(city3);
         }
         System.out.println(cities);
         }
